3f78a6420775846aa978ed4483626ba870e01323
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2026-02-16 23:44:54 -0500

    Initialize the asteroid game

---
+++ README.md
@@ -0,0 +1,5 @@
+# asteroid Game with Python and Pygame
+
+A guided project from [boot.dev](https://www.boot.dev/courses/build-asteroids-python)
+
+Started 2/15/2026
---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from constants import 
+
+def main():
+    print("Starting Asteroids with pygame version: 2.6.1")
+
+
+if __name__ == "__main__":
+    main()
