86fcd2c304ed16cac2d59561018e8f5bb6936bd8
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2026-02-19 00:54:13 -0500

    Move to a local Linux machine

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from logger import log_state
 
 def main():
 	print("Starting Asteroids with pygame version: 2.6.1")
 	print(f'Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}')
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	while True:
+		log_state()
+		for event in pygame.event.get():
+			pass
+		screen.fill('black')
+		pygame.display.flip()
 
 
 if __name__ == "__main__":
