e0164d8bbdb4779369dd4dc8676b33630bab88b1
Quang Hoang <126427971+theantigone@users.noreply.github.com>
2026-02-22 02:35:00 -0500

    Group the methods

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
@@ -18,7 +21,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        # player.update(dt)
+        updatable.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+        # player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
